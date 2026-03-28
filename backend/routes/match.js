eb80df9fc198082e684743705b8004d6b4330945
Vikash <your-pannelwork29@gmail.com>
2026-03-28 15:19:07 +0530

    final api fix

--- backend/routes/match.js
+++ backend/routes/match.js
@@ -3,15 +3,21 @@ const axios = require("axios");
 
 const router = express.Router();
 
-// ✅ LIVE MATCHES API
 router.get("/live-matches", async (req, res) => {
   try {
     const response = await axios.get(
       "https://cricket-live-line-advance.p.rapidapi.com/matches",
       {
+        params: {
+          status: "3",
+          per_page: "50",
+          paged: "1",
+          highlight_live_matches: "1",
+        },
         headers: {
           "X-RapidAPI-Key": "36c4538c48mshf03efb976f9f122p11bc25jsnde99b1826066",
           "X-RapidAPI-Host": "cricket-live-line-advance.p.rapidapi.com",
+          "Content-Type": "application/json",
         },
       }
     );
